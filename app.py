9d423c27ddfe488fb0274a698a851ba7901ce60d
Aviral <192993778+aviral-trivedi@users.noreply.github.com>
2025-01-19 23:47:39 +0530

    --allow-empty

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ bot = telebot.TeleBot(api_key)
 def send_welcome(message):
     bot.reply_to(message, "Howdy, how are you doing?")
 
+@bot.message_handler(commands=['help'])
+def send_help(message):
+    bot.reply_to(message, "I am just a simple bot figuring out what can I help people with.")
 
 @bot.message_handler(func=lambda msg: True)
 def echo_all(message):
