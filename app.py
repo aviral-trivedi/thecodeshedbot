22d49ccb2ce0d7563e76c750a5e7c82c9cd13b6c
Aviral <192993778+aviral-trivedi@users.noreply.github.com>
2025-01-19 23:21:08 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,18 @@
+import os
+import telebot
+
+api_key = os.getenv('CODESHED_BOT_TOKEN')
+bot = telebot.TeleBot(api_key)
+
+@bot.message_handler(commands=['start', 'hello'])
+def send_welcome(message):
+    bot.reply_to(message, "Howdy, how are you doing?")
+
+
+@bot.message_handler(func=lambda msg: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+
+
+bot.infinity_polling()
